1f0c5fb98ce0b2037c0437a8600099817e66e65a
VadimZhukov <parsiffallne@gmail.com>
2025-10-13 08:31:58 +0300

    fix linter

--- minerva/tools/fetch_html.py
+++ minerva/tools/fetch_html.py
@@ -1,32 +1,29 @@
-from typing import Any, Unpack, cast
+from typing import Unpack
 import httpx
 import lxml
 import lxml.html
-import lxml.html.clean
+from lxml.html import clean
 
 from minerva.tools.tool_kwargs import DefaultToolKwargs  # type: ignore
 
 TIMEOUT_SEC = 2
 
-LXML_CLEANER = cast(
-  Any,
-  lxml.html.clean.Cleaner(
-    scripts=True,
-    javascript=True,
-    comments=True,
-    style=True,
-    inline_style=True,
-    meta=True,
-    page_structure=False,
-    processing_instructions=True,
-    annoying_tags=True,
-    remove_tags=["html", "head", "body"],
-    kill_tags=["header", "footer"],
-    remove_unknown_tags=True,
-    # remove all attrs
-    safe_attrs_only=True,
-    safe_attrs=[],
-  ),
+LXML_CLEANER = clean.Cleaner(
+  scripts=True,
+  javascript=True,
+  comments=True,
+  style=True,
+  inline_style=True,
+  meta=True,
+  page_structure=False,
+  processing_instructions=True,
+  annoying_tags=True,
+  remove_tags=["html", "head", "body"],
+  kill_tags=["header", "footer"],
+  remove_unknown_tags=True,
+  # remove all attrs
+  safe_attrs_only=True,
+  safe_attrs=[],
 )
 
 
